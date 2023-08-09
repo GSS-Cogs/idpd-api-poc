60f2a97a300f49956a56dccf8b164e2e2d5f71a2
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-08-09 10:29:29 +0100

    added the proper header conditions

--- poc/hello_app.py
+++ poc/hello_app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@app.route("/", methods=['GET'])
+def main():
+    if request.method == 'GET':
+        if request.url.endswith(".json") or request.headers["Accept"] == "application/json":
+            return get_json(request)
+        elif request.url.endswith(".csv") or request.headers["Accept"] == "text/csv":
+            return get_csv(request)
+    else:
+        return "<h1 style='color:red'>ERROR!</h1>"
+
+def get_csv(request):
+    return "I’m a csv"
+
+def get_json(request):
+    return "I’m a json"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
