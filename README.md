d969c2ae6664fbafa6aa10b1139ef6b812cd053c
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-08-09 12:18:34 +0100

    reverted some of the changes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # idpd-api-poc
 A proof of concept API layer to manage content negotiation across IDPD resources and provide an abstraction to 3rd party data services
+
+## Usage
+
+- development mode: `pipenv run python3 ./app/hello_world.py` (this enables browser based exceptions and debugging)
+- productiond mode: `gunicorn --bind 0.0.0.0:5000 wsgi:app`
--- poc/hello_app.py
+++ poc/hello_app.py
@@ -3,21 +3,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
 def main():
-    if request.method == 'GET':
-        if request.url.endswith(".json") or request.headers["Accept"] == "application/json":
-            return get_json(request)
-        elif request.url.endswith(".csv") or request.headers["Accept"] == "text/csv":
-            return get_csv(request)
-    else:
-        return "<h1 style='color:red'>ERROR!</h1>"
+    return "<h1 style='Blue:red'>Hello World!</h1>"
 
-def get_csv(request):
-    return "I’m a csv"
-
-def get_json(request):
-    return "I’m a json"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
