2da8b7da321c06556813104bffa5dbf594813786
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-08-14 14:29:56 +0100

    changed to main

--- src/hello_app.py
+++ src/hello_app.py
@@ -1,13 +1,18 @@
 #this script set up the basic flask app
 
-from flask import Flask
+import re
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
 @app.route("/", methods =["GET"])
 def main():
-    return "<h1 style='Blue:red'>Hello World!</h1>"
+    if request.method == 'GET':
+        if request.url.endswith('json') or request.headers['Accept'] == "application/ld+json":
+            return '200'
+        else:
+            return '406'            
 
 
 if __name__ == "__main__":
