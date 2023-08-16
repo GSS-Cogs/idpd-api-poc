61dfb2c0a85669dfe6b330904a90982e442798ab
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-08-16 10:18:18 +0100

    added a second test to asser fails

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -3,7 +3,7 @@ import pytest
 from hello_app import app
 
 
-def test_hello_world_handler_for_some_behaviour_or_other():
+def test_hello_world_handler_for_some_behaviour():
     """
     test the hello_world handler to conform its doing
     a specific thing given a specific request
@@ -19,5 +19,25 @@ def test_hello_world_handler_for_some_behaviour_or_other():
     response = client.get('/', headers=headers)
 
     # Assertions etc
-    assert response.status_code == 200
+    assert response.text == "200"
+    assert 'text/html' == response.mimetype
+
+
+def test_hello_world_handler_for_some_behaviour_fail():
+    """
+    test the hello_world handler to conform it fails
+    when a not specified data type is provided
+    """
+
+    # Create a test client
+    client = app.test_client()
+
+    # # any headers you want, as a key value dict
+    headers = {"Accept":"something"}
+
+    # specify your end point
+    response = client.get('/', headers=headers)
+
+    # Assertions etc
+    assert response.text == "406"
     assert 'text/html' == response.mimetype
