88b2f73b873ba9154ce372a2c5b780c2c156973a
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-08-21 11:39:35 +0100

    added the query and the url

--- src/store/base.py
+++ src/store/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from sparql import run_sparql
+from SPARQLWrapper import SPARQLWrapper, JSON, QueryResult
 
 class BaseStore(ABC):
 
@@ -19,13 +19,58 @@ class BaseStore(ABC):
 
 class SparqlStore(BaseStore):
 
+    def __init__(self):
+        self.query = ""
+
     # Do any setup this client requires
     def setup(self):
         # set the url for sparql
-        self.url = # get it from an env var
+        self.url = "https://beta.gss-data.org.uk/sparql"
 
-    def get_datasets(self):
+    
+    def sparql_query(self) -> QueryResult:
+
+    #this is where the url to get the data will be passed in
+        sparql = SPARQLWrapper(self.url)
+
+    #this is where the query will be passed in
+        sparql.setQuery("""
+        PREFIX dcat: <http://www.w3.org/ns/dcat#>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX gss: <http://gss-data.org.uk/catalog/>
+        PREFIX pmd: <http://publishmydata.com/pmdcat#>
+        SELECT DISTINCT * 
+        WHERE { gss:datasets dcat:record ?record .
+                ?record foaf:primaryTopic ?dataset .
+            ?dataset    dcterms:issued ?issued ;
+                dcterms:modified ?modified .
+            optional {  ?dataset    rdfs:label      ?name} 
+	        optional {  ?dataset    pmd:markdownDescription      ?description} 
+	        optional {  ?dataset    rdfs:comment      ?comment} 
+	        optional {  ?dataset    dcterms:license ?license .
+				?license    rdfs:label      ?licenseName} 
+            optional {  ?dataset    dcterms:creator ?creator .
+                ?creator    rdfs:label      ?creatorName} 
+            optional {  ?dataset    dcat:theme      ?theme .
+                ?theme      rdfs:label      ?themeName} 
+            }
+            ORDER BY ASC (?name) 
+            LIMIT 500""")
+
+    #not sure if this is needed
+        sparql.setReturnFormat(JSON)
+
+        results = sparql.query()
+
+        return results
+
+    def get_datasets(self) -> QueryResult:
         # do stuff
         # use self.url or anything else you set in setup
+        self.query = "this is where whe query will be passed in which will be the endpoint"
 
-        result = run_sparql(self.url)
+        result = self.sparql_query()
+
+        return result 
