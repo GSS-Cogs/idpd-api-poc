ad67a41585d301374fb0b960081a32d892632f61
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-08-25 09:57:53 +0100

    made changes requested

--- tests/test_sparql_store.py
+++ tests/test_sparql_store.py
@@ -5,58 +5,62 @@ import pytest
 
 from store.sparql import SparqlStore
 
+@pytest.fixture
+def configure_environment():
+    """
+    This is test setup, it removes any existing custom 
+    sparql endpoint specified by the user (an environment
+    variable) for the length of each test before restoring it.
 
-def test_get_datasets():
+    To use, you just pass it into the test function.
+    """
+
+    # Keep track of the original value (if any)
+    original_sparql_endpoint_env_var = os.environ.get("SPARQL_ENDPOINT_URL")
+
+    # If there was an original value, delete it
+    if original_sparql_endpoint_env_var is not None:
+        del os.environ["SPARQL_ENDPOINT_URL"]
+
+    # This yields (runs) the test
+    yield 
+
+    # Now the test has ran, if a custom env var was set - put it back
+    if original_sparql_endpoint_env_var:
+        os.environ["SPARQL_ENDPOINT_URL"] = original_sparql_endpoint_env_var
+
+def test_get_datasets(configure_environment):
     """testing that the get_dataset will return access
     the url provided and run the SPRQL query, returns a Dictionary"""
 
     test_instance = SparqlStore()
-
     result = test_instance.get_datasets()
-
     assert isinstance(result, Dict)
 
-def test_run_sparql():
+def test_run_sparql(configure_environment):
     """testing that the run_sparql will return a QueryResult object"""
     
     test_instance = SparqlStore()
-
     query = "SELECT * WHERE { ?s ?p ?o . } LIMIT 10"
-
     result = test_instance.run_sparql(query)
-
     assert isinstance(result, QueryResult)
 
-
-def test_sparql_store_setup_default():
+def test_sparql_store_setup_default(configure_environment):
     """
     testing the setup function having the correct url by default
     (when the env does not have a SPARQL_ENDPOINT_URL)
     """
 
     test_instance = SparqlStore()
-
     assert test_instance.sparql.endpoint == "https://beta.gss-data.org.uk/sparql"
     
-def test_sparql_store_setup_custom():
+def test_sparql_store_setup_custom(configure_environment):
     """
     testing the setup function having the correct 
     url when provided a custom env variable
     """
-    temporary_storage= None
-    #checking if user has already a SPARQL_ENDPOINT_URL 
-    if os.environ.get("SPARQL_ENDPOINT_URL") is not None:
-        #temporeraly saving the value
-        temporary_storage = os.environ.get("SPARQL_ENDPOINT_URL")
-        os.environ["SPARQL_ENDPOINT_URL"] = "https://beta.gss-data.org.uk/custom_endpoint"
-    else:
-        os.environ["SPARQL_ENDPOINT_URL"] = "https://beta.gss-data.org.uk/custom_endpoint"
 
+    os.environ["SPARQL_ENDPOINT_URL"] = "https://beta.gss-data.org.uk/custom_endpoint"
     test_instance = SparqlStore()
-
     assert test_instance.sparql.endpoint == "https://beta.gss-data.org.uk/custom_endpoint"
-
-    #re-setting the value back to what it was
-    if temporary_storage is not None:
-        os.environ["SPARQL_ENDPOINT_URL"] = temporary_storage
     
