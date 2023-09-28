9223831803b5b248a45e4bb2f2544c43ffab9c25
mikeAdamss <mikelovesbooks@gmail.com>
2023-09-28 13:32:05 +0100

    clarify endpoints

--- src/main.py
+++ src/main.py
@@ -41,77 +41,7 @@ def datasets(request: Request, response: Response):
         return
 
 @app.get("/datasets/{id}")
-def dataset_by_id(request: Request, response: Response, id: str):
-    metadata_store = app.state.stores["dataset_metadata"]
-    if request.headers['Accept'] == JSONLD or BROWSABLE:
-        datasets = metadata_store.get_dataset_by_id(id)
-        if len(datasets) == 1:
-            response.status_code = status.HTTP_200_OK
-            return datasets[0]
-        else:
-            response.status_code = status.HTTP_404_NOT_FOUND
-            return
-    else:
-        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
-        return
-    
-@app.get("/publishers")
-def publishers(request: Request, response: Response):
-    metadata_store = app.state.stores["publishers_metadata"]
-    if request.headers['Accept'] == JSONLD or BROWSABLE:
-        response.status_code = status.HTTP_200_OK
-        return metadata_store.get_publishers()
-    else:
-        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
-        return
-
-@app.get("/publishers/{id}")
-def publisher_by_id(request: Request, response: Response, id: str):
-    metadata_store = app.state.stores["publisher_metadata"]
-    if request.headers['Accept'] == JSONLD or BROWSABLE:
-        publishers = metadata_store.get_publisher_by_id(id)
-        if len(publishers) == 1:
-            response.status_code = status.HTTP_200_OK
-            return publishers[0]
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return
-    else:
-        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
-        return
-    
-@app.get("/topics")
-def topics(request: Request, response: Response):
-    metadata_store = app.state.stores["topics_metadata"]
-    if request.headers['Accept'] == JSONLD or BROWSABLE:
-        response.status_code = status.HTTP_200_OK
-        return metadata_store.get_topics()
-    else:
-        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
-        return
-
-@app.get("/topics/{id}")
-def topic_by_id(request: Request, response: Response, id: str):
-    metadata_store = app.state.stores["topic_metadata"]
-    if request.headers['Accept'] == JSONLD or BROWSABLE:
-        themes = metadata_store.get_topic_by_id(id)
-        if len(themes) == 1:
-            response.status_code = status.HTTP_200_OK
-            return themes[0]
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return
-    else:
-        response.status_code = status.HTTP_406_NOT_ACCEPTABLE
-        return
-
-"""not for included in pr, but to include in prdescrition"""
-@app.get("/datasets/{dataset_id}/editions/{edition_id}/versions/{version_id}")
-def get_csv(dataset_id: str, edition_id: str, version_id: str):
-    csv_store = app.state.stores["version_csv"]
-    data = csv_store.get_version(dataset_id, edition_id, version_id)
-    return data
-=======
-@app.get("/datasets/{id}")
-def dataset_by_id(request: Request, response: Response, id: str):
+def dataset(request: Request, response: Response, id: str):
     metadata_store = app.state.stores["dataset_metadata"]
     if request.headers['Accept'] == JSONLD or BROWSABLE:
         datasets = metadata_store.get_dataset(id)
@@ -124,7 +54,13 @@ def dataset_by_id(request: Request, response: Response, id: str):
     else:
         response.status_code = status.HTTP_406_NOT_ACCEPTABLE
         return
-    
+
+@app.get("/datasets/{dataset_id}/editions/{edition_id}/versions/{version_id}")
+def version(dataset_id: str, edition_id: str, version_id: str):
+    csv_store = app.state.stores["version_csv"]
+    data = csv_store.get_version(dataset_id, edition_id, version_id)
+    return data
+
 @app.get("/publishers")
 def publishers(request: Request, response: Response):
     metadata_store = app.state.stores["publishers_metadata"]
@@ -136,7 +72,7 @@ def publishers(request: Request, response: Response):
         return
 
 @app.get("/publishers/{id}")
-def publisher_by_id(request: Request, response: Response, id: str):
+def publisher(request: Request, response: Response, id: str):
     metadata_store = app.state.stores["publisher_metadata"]
     if request.headers['Accept'] == JSONLD or BROWSABLE:
         publishers = metadata_store.get_publisher(id)
@@ -160,7 +96,7 @@ def topics(request: Request, response: Response):
         return
 
 @app.get("/topics/{id}")
-def topic_by_id(request: Request, response: Response, id: str):
+def topic(request: Request, response: Response, id: str):
     metadata_store = app.state.stores["topic_metadata"]
     if request.headers['Accept'] == JSONLD or BROWSABLE:
         themes = metadata_store.get_topic(id)
