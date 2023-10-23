f40aacbcf26e44f7810151a866a8a007a3cd82f8
Moasib-Arif <moasib_arif@yahoo.com>
2023-10-23 12:59:13 +0100

    Made the requested changes

--- src/main.py
+++ src/main.py
@@ -1,73 +1,32 @@
-import logging
 import os
 from typing import Optional
 
 from fastapi import Depends, FastAPI, Request, Response, status
-import structlog
 
 from constants import JSONLD
 import schemas
 from store import StubCsvStore, StubMetadataStore
 
-from custom_logging import configure_logger, logger
-from structlog.contextvars import bind_contextvars, clear_contextvars
-import time
+from custom_logging import logger
+from middleware import logging_middleware 
+
 # Simple env var flag to allow local browsing of api responses
 # while developing.
 BROWSABLE = os.environ.get("LOCAL_BROWSE_API")
 
 app = FastAPI()
-configure_logger()
+
+# Add the logging middleware to the app
+app.middleware("http")(logging_middleware)
 
 
-@app.middleware("http")
-async def logging_middleware(request: Request, call_next) -> Response:
-    clear_contextvars()
-    # These context vars will be added to all log entries emitted during the request
-    request_id = request.headers.get("request-id")
-    bind_contextvars(request_id=request_id)
-
-    start_time = time.perf_counter_ns()
-
-    try:
-        response = await call_next(request)
-    except Exception as exc:
-        response = Response(status_code=500)
-        structlog.get_logger("api.error").exception("Uncaught exception", exc_info=exc)
-        raise
-    finally:
-        process_time = time.perf_counter_ns() - start_time
-        status_code = response.status_code
-
-    log_message = "Request completed"
-    log_context = {
-        "path": request.url.path,
-        "method": request.method,
-        "status_code": status_code,
-        "process_time": process_time / 10 ** 9,  # Convert to seconds
-    }
-
-    if status_code == 200:
-        logger.info(log_message, **log_context)
-    elif status_code == 404:
-        log_message = "Resource not found"
-        logger.info(log_message, **log_context)
-    elif status_code == 406:
-        log_message = "Request not acceptable"
-        logger.error(log_message, **log_context)
-
-
-        response.headers["X-Process-Time"] = str(process_time / 10 ** 9)
-
-    return response
-    
 @app.get("/datasets", response_model=Optional[schemas.Datasets])
 async def datasets(
     request: Request,
     response: Response,
     metadata_store: StubMetadataStore = Depends(StubMetadataStore),
 ):
-    logger.info("Received request for datasets")
+    logger.info("Received request for datasets", data={"request_type": "datasets"})
        
     if request.headers["Accept"] == JSONLD or BROWSABLE:
         response.status_code = status.HTTP_200_OK
@@ -81,7 +40,6 @@ async def datasets(
         response.status_code = status.HTTP_406_NOT_ACCEPTABLE
         return
 
-
 @app.get("/datasets/{dataset_id}", response_model=Optional[schemas.Dataset])
 async def dataset(
     request: Request,
@@ -89,7 +47,7 @@ async def dataset(
     dataset_id: str,
     metadata_store: StubMetadataStore = Depends(StubMetadataStore),
 ):
-    logger.info(f"Received request for dataset with ID: {dataset_id}", dataset_id)
+    logger.info("Received request for dataset with ID", data={"dataset_id": dataset_id})
     if request.headers["Accept"] == JSONLD or BROWSABLE:
         dataset = metadata_store.get_dataset(dataset_id)
         if dataset is not None:
@@ -109,7 +67,7 @@ async def editions(
     dataset_id: str,
     metadata_store: StubMetadataStore = Depends(StubMetadataStore),
 ):
-    logger.info(f"Received request for dataset (ID: {dataset_id}) editions.")
+    logger.info("Received request for dataset editions", data={"dataset_id": dataset_id})
     if request.headers["Accept"] == JSONLD or BROWSABLE:
         editions = metadata_store.get_editions(dataset_id)
         if editions is not None:
@@ -130,7 +88,7 @@ async def edition(
     edition_id: str,
     metadata_store: StubMetadataStore = Depends(StubMetadataStore),
 ):
-    logger.info(f"Received request for dataset (ID: {dataset_id}) and edition (ID: {edition_id}).")
+    logger.info("Received request for dataset and edition", data={"dataset_id": dataset_id, "edition_id": edition_id})
     if request.headers["Accept"] == JSONLD or BROWSABLE:
         edition = metadata_store.get_edition(dataset_id, edition_id)
         if edition is not None:
@@ -149,7 +107,7 @@ async def publishers(
     response: Response,
     metadata_store: StubMetadataStore = Depends(StubMetadataStore),
 ):
-    logger.info("Received request for publishers")
+    logger.info("Received request for publishers", data={"request_type": "publishers"})
     if request.headers["Accept"] == JSONLD or BROWSABLE:
         response.status_code = status.HTTP_200_OK
         publishers = metadata_store.get_publishers()
@@ -170,7 +128,7 @@ async def publisher(
     publisher_id: str,
     metadata_store: StubMetadataStore = Depends(StubMetadataStore),
 ):
-    logger.info(f"Received request for publisher with ID: {publisher_id}", publisher_id)
+    logger.info("Received request for publisher with ID", data={"publisher_id": publisher_id})
     if request.headers["Accept"] == JSONLD or BROWSABLE:
         publisher = metadata_store.get_publisher(publisher_id)
         if publisher is not None:
@@ -189,7 +147,7 @@ async def topics(
     response: Response,
     metadata_store: StubMetadataStore = Depends(StubMetadataStore),
 ):
-    logger.info("Received request for publishers")
+    logger.info("Received request for topics", data={"request_type": "topics"})
     if request.headers["Accept"] == JSONLD or BROWSABLE:
         response.status_code = status.HTTP_200_OK
         topics = metadata_store.get_topics()
@@ -210,7 +168,7 @@ async def topic(
     topic_id: str,
     metadata_store: StubMetadataStore = Depends(StubMetadataStore),
 ):
-    logger.info(f"Received request for topic with ID: {topic_id}", topic_id)
+    logger.info("Received request for topic with ID", data={"topic_id": topic_id})
     if request.headers["Accept"] == JSONLD or BROWSABLE:
         topic = metadata_store.get_topic(topic_id)
         if topic is not None:
@@ -222,8 +180,7 @@ async def topic(
         response.status_code = status.HTTP_406_NOT_ACCEPTABLE
         return
 
-
-# note: download only for now, needs expanding
+# Note: download only for now, needs expanding
 @app.get("/datasets/{dataset_id}/editions/{edition_id}/versions/{version_id}")
 async def version(
     request: Request,
@@ -233,7 +190,7 @@ async def version(
     version_id: str,
     csv_store: StubCsvStore = Depends(StubCsvStore),
 ):
-    logger.info(f"Received request for dataset (ID: {dataset_id}), edition (ID: {edition_id}), and version (ID: {version_id}).")
+    logger.info("Received request for dataset, edition, and version", data={"dataset_id": dataset_id, "edition_id": edition_id, "version_id": version_id})
     csv_data = csv_store.get_version(dataset_id, edition_id, version_id)
     if csv_data is not None:
         response.status_code = status.HTTP_200_OK
