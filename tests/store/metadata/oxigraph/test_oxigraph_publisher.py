6336d40777ddde06c2b4ed726721aaf19438808a
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-11-02 11:51:06 +0000

    added an invalid test

--- tests/store/metadata/oxigraph/test_oxigraph_publisher.py
+++ tests/store/metadata/oxigraph/test_oxigraph_publisher.py
@@ -27,3 +27,21 @@ def test_oxigraph_get_publisher_returns_valid_structure():
 
      # So should not raise
      schemas.Publisher(**publisher["@graph"][0])
+
+
+def test_oxigraph_get_publisher_returns_invalid_structure():
+     """
+     Confirm that the OxigrapgMetadataStore.get_dataset()
+     function returns a dataset that matches the dataset
+     schema.
+     """
+
+     os.environ["GRAPH_DB_URL"] = "http://localhost:7878"
+     store = OxigraphMetadataStore()
+
+     publisher = store.get_publisher("office-for-national-statistics")
+
+     # Sanity check that the schema validation is working as intended
+     # i.e raises with wrong structure
+     with pytest.raises(ValidationError):
+        schemas.Dataset(**{"I": "break"})
