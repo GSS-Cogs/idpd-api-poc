8e23f4db2e1aca7d0c3d488567d0538a014980f2
muazzam <muazzam.chaudhary@gsscogs.uk>
2023-11-20 08:21:36 +0000

    resolving merge conflicts

---
+++ tests/store/metadata/oxigraph/test_oxigraph_publishers.py
@@ -0,0 +1,28 @@
+import pytest
+
+from pydantic import ValidationError
+
+from store.metadata.oxigraph.store import OxigraphMetadataStore
+import schemas
+
+
+def test_oxigraph_get_publishers_valid_return():
+    """
+    Confirm that the OxigraphMetadataStore.get_publishers()
+    function returns a list of publishers that matches the Publishers
+    schema.
+    """
+
+    store = OxigraphMetadataStore()
+    publishers = store.get_publishers()
+    publishers_schema = schemas.Publishers(**publishers)
+    assert publishers_schema.id == "https://staging.idpd.uk/publishers"
+    assert publishers_schema.type == "hydra:Collection"
+    assert publishers_schema.count == 3
+    assert publishers_schema.offset == 0
+
+
+def test_topics_schema_validation():
+    """Confirm that the schema validation is working as intended i.e raises ValidationError with wrong structure"""
+    with pytest.raises(ValidationError):
+        schemas.Publishers(**{"I": "break"})
