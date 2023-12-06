5cd49cbec0f4188e4b77d6c676a51e7ed0b6059b
Jozsef K <43893201+nimshi89@users.noreply.github.com>
2023-12-06 11:23:28 +0000

    changed tests and decorator

--- src/validation.py
+++ src/validation.py
@@ -1,15 +1,15 @@
 from sqlite3 import Date
-from pydantic import validator
+from pydantic import validate_call
 
 from datetime import datetime
 
 
 
-@validator('time')
-def validate_time(cls, time_value):
+@validate_call
+def validate_time(time_value):
     try:
         #Format should be "YYYY-MM-DD %H%M%S"
         datetime.strptime(time_value, '%Y-%m-%d %H:%M:%S')
         return time_value
     except ValueError:
-        return ValueError
+        return ValueError("The valid time format is 'YYYY-MM-DD %H%M%S'")
--- tests/store/metadata/oxigraph/test_datetime.py
+++ tests/store/metadata/oxigraph/test_datetime.py
@@ -2,9 +2,19 @@ from src.validation import validate_time
 
 
 
-def test_check_time_validation():
+def test_check_time_validation_valid():
     """this funciton will test the pydantic time validation"""
 
     time_value = "2023-11-29 16:00:00"
 
-    assert validate_time
+    assert validate_time(time_value) == "2023-11-29 16:00:00"
+
+def test_check_time_validation_unvalid():
+    """this funciton will test the pydantic time
+     validation with an unvalid value"""
+
+    time = "2023-11-SS 16:00:00"
+    try:
+        raise validate_time(time)
+    except ValueError as x:
+        x == "The valid time format is 'YYYY-MM-DD %H%M%S'"
