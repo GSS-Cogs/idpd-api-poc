84cefe4a16e36c71143bf6092770a59fe9ea864c
muazzam <muazzam.chaudhary@gsscogs.uk>
2023-12-14 08:03:02 +0000

    added rest of the unit tests

--- tests/test_custom_logging.py
+++ tests/test_custom_logging.py
@@ -1,3 +1,5 @@
+import json
+from pathlib import Path
 from typing import Optional
 from structlog.testing import capture_logs
 from unittest.mock import MagicMock
@@ -12,11 +14,18 @@ from main import app, StubMetadataStore
 from tests.fixtures.versions import versions_test_data
 
 from store.metadata.oxigraph.store import OxigraphMetadataStore
+from store.metadata.context import ContextStore
 import schemas
+from starlette.responses import FileResponse
+
+from store.csv.stub.store import StubCsvStore
+
+from store.metadata.stub.store import StubMetadataStore
+from src import schemas
 
 ENDPOINT = "datasets/some-dataset-id/editions/some-edition-id/versions"
 
-def test_logger_request_id():
+def test_request_handlers_return_logger_request_id():
     """
     Confirms that:
 
@@ -39,7 +48,7 @@ def test_logger_request_id():
             if 'request_id' in log:
                 assert 'log_level' in log
                 assert log.get('log_level') == 'info'
-                assert type(log.get('request_id')) == Optional[int]
+                assert type(log.get('request_id')) == str
 
 def test_oxigraph_get_datasets_logger_returns_request_id():
     """
@@ -57,18 +66,116 @@ def test_oxigraph_get_datasets_logger_returns_request_id():
             assert log.get('log_level') == 'info'
             # assert type(log.get('request_id')) == None
 
-def test_oxigraph_get_versions_returns_valid_structure():
+def test_oxigraph_get_versions_logger_request_id_is_none():
     """
     Confirm that the OxigraphMetadataStore.get_versions()
     function returns a dictionary that matches the Versions
     schema.
     """
-    store = OxigraphMetadataStore()
-    versions = store.get_versions("4gc", "2023-09")
-    versions_schema = schemas.Versions(**versions)
-    assert (
-        versions_schema.id
-        == "https://staging.idpd.uk/datasets/4gc/editions/2023-09/versions"
-    )
-    assert versions_schema.type == "hydra:Collection"
-    assert len(versions_schema.versions) == versions_schema.count
+    with capture_logs() as cap_logs: 
+        store = OxigraphMetadataStore()
+        versions = store.get_versions("4gc", "2023-09", request_id=None)
+        versions_schema = schemas.Versions(**versions)
+        
+        for log in cap_logs:
+            # if 'request_id' in log:
+            assert 'request_id' in log
+            assert 'log_level' in log
+            assert log.get('log_level') == 'info'
+            # assert type(log.get('request_id')) == None
+
+def test_context_store_logger_returns_request_id():
+    with capture_logs() as cap_logs: 
+        context_store = ContextStore()
+        context = context_store.get_context(request_id=3)
+
+        for log in cap_logs:
+                # if 'request_id' in log:
+                assert 'request_id' in log
+                assert 'log_level' in log
+                assert log.get('log_level') == 'info'
+                # assert type(log.get('request_id')) == None
+
+def test_context_store_logger_request_id_is_none():
+    with capture_logs() as cap_logs: 
+        context_store = ContextStore()
+        context = context_store.get_context(request_id=None)
+
+        for log in cap_logs:
+                # if 'request_id' in log:
+                assert 'request_id' in log
+                assert 'log_level' in log
+                assert log.get('log_level') == 'info'
+                # assert type(log.get('request_id')) == None
+
+def test_csv_stub_logger_returns_request_id():
+    """
+    Just test the store can return a csv when we
+    ask for a csv that we know exists but do include
+    the ".csv" file extensions.
+    """
+    with capture_logs() as cap_logs:
+        csv_store = StubCsvStore()
+        csv = csv_store.get_version(request_id=3)
+
+        for log in cap_logs:
+                # if 'request_id' in log:
+                assert 'request_id' in log
+                assert 'log_level' in log
+                assert log.get('log_level') == 'info'
+                # assert type(log.get('request_id')) == None
+
+def test_csv_stub_logger_request_id_is_none():
+    """
+    Just test the store can return a csv when we
+    ask for a csv that we know exists but do include
+    the ".csv" file extensions.
+    """
+    with capture_logs() as cap_logs:
+        csv_store = StubCsvStore()
+        csv = csv_store.get_version(request_id=None)
+
+        for log in cap_logs:
+                # if 'request_id' in log:
+                assert 'request_id' in log
+                assert 'log_level' in log
+                assert log.get('log_level') == 'info'
+                # assert type(log.get('request_id')) == None
+
+def test_stub_get_dataset_logger_returns_request_id():
+    """
+    Confirm that the OxigrapgMetadataStore.get_dataset()
+    function returns a dataset that matches the dataset
+    schema.
+    """
+
+    with capture_logs() as cap_logs:
+        store = StubMetadataStore()
+
+        dataset = store.get_dataset("cpih",request_id=3)
+
+        for log in cap_logs:
+                # if 'request_id' in log:
+                assert 'request_id' in log
+                assert 'log_level' in log
+                assert log.get('log_level') == 'info'
+                # assert type(log.get('request_id')) == None
+
+def test_stub_get_datasets_logger_request_id_is_none():
+    """
+    Confirm that the OxigrapgMetadataStore.get_dataset()
+    function returns a dataset that matches the dataset
+    schema.
+    """
+
+    with capture_logs() as cap_logs:
+        store = StubMetadataStore()
+
+        datasets = store.get_datasets(request_id=None)
+
+        for log in cap_logs:
+                # if 'request_id' in log:
+                assert 'request_id' in log
+                assert 'log_level' in log
+                assert log.get('log_level') == 'info'
+                # assert type(log.get('request_id')) == None
