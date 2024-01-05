acbb6b9689c691fd3f831615deb7216be1cabbbb
mike <mikelovesbooks@gmail.com>
2024-01-05 11:43:46 +0000

    commented out graph related tests

--- tests/test_custom_logging.py
+++ tests/test_custom_logging.py
@@ -70,44 +70,44 @@ def test_stub_get_versions_return_none_for_request_id_in_store_logs():
                 assert log.get('log_level') == 'info'
                 assert type(log.get('request_id')) == type(None)
 
-def test_oxigraph_get_datasets_return_request_id_in_store_log():
-    """
-    Confirms that:
+# def test_oxigraph_get_datasets_return_request_id_in_store_log():
+#     """
+#     Confirms that:
 
-    - Request ID is pass into store OxigraphMetadataStore methods
-    - All OxigraphMetadataStore loggers are logging with request ID
-    """  
-    with capture_logs() as cap_logs:    
-        store = OxigraphMetadataStore()
-        datasets = store.get_datasets(request_id='test_request_id_625')
-        datasets_schema = schemas.Datasets(**datasets)
+#     - Request ID is pass into store OxigraphMetadataStore methods
+#     - All OxigraphMetadataStore loggers are logging with request ID
+#     """  
+#     with capture_logs() as cap_logs:    
+#         store = OxigraphMetadataStore()
+#         datasets = store.get_datasets(request_id='test_request_id_625')
+#         datasets_schema = schemas.Datasets(**datasets)
 
-        for log in cap_logs:
-            if log.get('event') == 'Constructing get_datasets() response from graph':
-                assert 'request_id' in log
-                assert 'log_level' in log
-                assert log.get('log_level') == 'info'
-                assert type(log.get('request_id')) == str
-                assert log.get('request_id') == "test_request_id_625"
+#         for log in cap_logs:
+#             if log.get('event') == 'Constructing get_datasets() response from graph':
+#                 assert 'request_id' in log
+#                 assert 'log_level' in log
+#                 assert log.get('log_level') == 'info'
+#                 assert type(log.get('request_id')) == str
+#                 assert log.get('request_id') == "test_request_id_625"
 
-def test_oxigraph_get_versions_return_none_for_request_id_in_store_log():
-    """
-    Confirms that:
+# def test_oxigraph_get_versions_return_none_for_request_id_in_store_log():
+#     """
+#     Confirms that:
 
-    - Request ID is pass into OxigraphMetadataStore methods
-    - All OxigraphMetadataStore loggers are logging with request ID, even if request_id=None
-    """ 
-    with capture_logs() as cap_logs: 
-        store = OxigraphMetadataStore()
-        versions = store.get_versions("4gc", "2023-03", request_id=None)
-        versions_schema = schemas.Versions(**versions)
+#     - Request ID is pass into OxigraphMetadataStore methods
+#     - All OxigraphMetadataStore loggers are logging with request ID, even if request_id=None
+#     """ 
+#     with capture_logs() as cap_logs: 
+#         store = OxigraphMetadataStore()
+#         versions = store.get_versions("4gc", "2023-03", request_id=None)
+#         versions_schema = schemas.Versions(**versions)
         
-        for log in cap_logs:
-            if log.get('event') == 'Constructing get_versions() response from graph':
-                assert 'request_id' in log
-                assert 'log_level' in log
-                assert log.get('log_level') == 'info'
-                assert type(log.get('request_id')) == type(None)
+#         for log in cap_logs:
+#             if log.get('event') == 'Constructing get_versions() response from graph':
+#                 assert 'request_id' in log
+#                 assert 'log_level' in log
+#                 assert log.get('log_level') == 'info'
+#                 assert type(log.get('request_id')) == type(None)
 
 
 def test_context_store_return_request_id_in_store_log():
